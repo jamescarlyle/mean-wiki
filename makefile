9ada2b5f367181a3f68799d6034fe6edeb08f93b
jamescarlyle <jwgcarlyle@gmail.com>
2015-01-12 17:44:40 +0000

    separated client and server code

--- makefile
+++
@@ -1,3 +0,0 @@
-test:
-	mocha --recursive --watch --require ./public/lib/angular.min.js --reporter list ./public/js/test/*.test.js
-.PHONY: test
--- test/conf.js
+++
@@ -1,9 +0,0 @@
-// need to serve the page locally over http, e.g. using python -m SimpleHTTPServer 8000
-// need to start selenium via webdriver e.g. webdriver-manager start
-// conf.js
-exports.config = {
-	seleniumAddress: 'http://localhost:4444/wd/hub',
-	// chromeOnly true causes direct interaction with Chrome, and webdriver/Selenium is not required
-	chromeOnly: true,
-	specs: ['spec.js']
-}
